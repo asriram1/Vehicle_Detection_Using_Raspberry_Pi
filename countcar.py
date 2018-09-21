fc580d992555bfe28e3e3bd50d56b752e9584e38
asriram1 <asriram@bu.edu>
2018-09-20 23:54:20 -0400

    Update countcar.py

--- countcar.py
+++ countcar.py
@@ -1,35 +1,27 @@
-# OpenCV Python program to detect cars in video frame
 # import libraries of python OpenCV 
 import cv2
- 
-# capture frames from a video
-cap = cv2.VideoCapture('videonew3.mp4')
- 
-# Trained XML classifiers describes some features of some object we want to detect
-car_cascade = cv2.CascadeClassifier('cars.xml')
- 
-# loop runs if capturing has been initialized.
-while True:
-    # reads frames from a video
-    ret, frames = cap.read()
+capture_frames = cv2.VideoCapture('videonew3.mp4')# capture frames from video recorded through raspberry pi 
+car_casc = cv2.CascadeClassifier('cars.xml') # Trained XML classifier describes some features of the object, in this case cars, that we want to detect
+
+while True: # loop runs if capturing has been initialized.
+
+    ret, frames = capture_frames.read() # reads frames from a video
      
-    # convert to gray scale of each frames
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY) # function converts each frame to gray scale 
      
  
-    # Detects cars of different sizes in the input image
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    cars = car_casc.detectMultiScale(gray, 1.1, 1)# MultiScale function detects cars of different sizes in the input images
      
-    # To draw a rectangle in each cars
+    # for loop to create rectangles around each of the cars
     for (x,y,w,h) in cars:
         cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
  
-   # Display frames in a window 
+   # imshow function to display frames in a window 
     cv2.imshow('video2', frames)
      
-    # Wait for Esc key to stop
+    # waitKey function waits for Esc key to stop
     if cv2.waitKey(33) == 27:
         break
  
-#De-allocate any associated memory usage
+#This de-allocates any associated memory usage
 cv2.destroyAllWindows()
